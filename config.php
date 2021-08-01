9604261f3ef751f4e03a0267ea1598540e1872e5
Nikita <xrestrus@gmail.com>
2021-08-01 18:50:22 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,19 @@
 <?php
 
-define('HOST', 'localhost');
-define('USER', 'root');
-define('PASSWORD', 'root');
-define('DATABASENAME', 'fake_db');
+//Разработка
+// define('HOST', 'localhost');
+// define('USER', 'root');
+// define('PASSWORD', 'root');
+// define('DATABASENAME', 'fake_db');
+
+$url = parse_url(getenv("CLEARDB_CYAN_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+define('HOST', $server);
+define('USER', $username);
+define('PASSWORD', $password);
+define('DATABASENAME', $db);
